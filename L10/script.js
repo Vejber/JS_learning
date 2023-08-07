cba4bda1a3c9e78a7eedefcef531e76b15fc4ae1
Vejber <105496392+Vejber@users.noreply.github.com>
2023-08-07 16:28:08 +0500

    -

--- L10/script.js
+++ L10/script.js
@@ -258,3 +258,19 @@ function uniteArrays(){
 
     console.log(dictionary);
 }
+
+
+const createCounter = () => {
+    let counter = 0;
+    return () => {
+        return ++counter;
+    }
+}
+// Создаем счетчик.
+const counter1 = createCounter();
+counter1(); // 1
+counter1(); // 2
+//Создадим еще один счетчик. Каждый будет работать независимо.
+const counter2 = createCounter();
+counter2(); // 1
+counter1(); // 3
